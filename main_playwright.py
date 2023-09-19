c4a8fa166e185476763d5cbecc10a193696e11e4
pablo <pcusnir@gmail.com>
2023-09-19 17:11:17 +0300

    Removed unused code

--- main_playwright.py
+++ main_playwright.py
@@ -11,10 +11,10 @@ def find_kamiq(playwright):
     # browser = chromium.launch()
     page = browser.new_page()
 
-    page.goto("https://playwright.dev/")
+    # page.goto("https://playwright.dev/")
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+    # expect(page).to_have_title(re.compile("Playwright"))
 
     auto_deal = "https://www.auto-deal.co.il/%d7%aa%d7%95%d7%a6%d7%90%d7%95%d7%aa-%d7%97%d7%99%d7%a4%d7%95%d7%a9/"
     page.goto(auto_deal)
@@ -26,14 +26,14 @@ def find_kamiq(playwright):
     pp = mm[0].split('\n')
     # page.get_by_label("filter-by-make-input").select_option(value="98")
     # print(ll)
+    print(pp)
     if 'פאביה' in pp:
         print('FABIA!!!')
         page.locator("#filter-by-model-input").select_option(value="185")
-        time.sleep(10)
+        time.sleep(5)
 
     if 'קאמיק' in pp:
         print('KAMIQ!!!')
-    print(pp)
 
 
 if __name__ == '__main__':
